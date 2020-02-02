2b9957c33de4f05e657ea1e4d987543e7db7b799
pthulen <pfthulen@gmail.com>
2020-02-02 13:40:06 -0800

    First commit, basic html frame

---
+++ public/script.js
@@ -0,0 +1,5 @@
+const fetchAllButton = document.getElementById('fetch-quotes');
+const fetchRandomButton = document.getElementById('fetch-random');
+const fetchByAuthorButton = document.getElementById('fetch-by-author');
+
+const quoteContainer = document.getElementById('quote-container');
