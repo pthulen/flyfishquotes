9702161ba480057aabb49d93c28c67d045ddad6b
pthulen <pfthulen@gmail.com>
2020-02-02 14:26:00 -0800

    Added quotes to data file to be accessed by API

---
+++ data.js
@@ -0,0 +1,87 @@
+const quotes = [
+    {
+      quote: 'If people don’t occasionally walk away from you shaking their heads, you’re doing something wrong.',
+      person: 'John Gierach'
+    },
+    {
+      quote: 'Be patient and calm – for no one can catch fish in anger. best thing about a boolean is even if you are wrong, you are only off by a bit.',
+      person: 'Herbert Hoover'
+    },
+    {
+      quote: `I frankly don’t make much of a living, but I make a hell of a life`,
+      person: 'Jack Gartside'
+    },
+    {
+      quote: 'Soon after I embraced the sport of angling I became convinced that I should never be able to enjoy it if I had to rely on the cooperation of the fish.',
+      person: 'Sparse Grey Hackle'
+    },
+    {
+      quote: 'Some go to church and think about fishing, others go fishing and think about God.',
+      person: 'Tony Blake'
+    },
+    {
+      quote: 'The only reason I ever played golf in the first place was so that I could afford to hunt and fish.',
+      person: 'Sam Snead'
+    },
+    {
+      quote: 'Scholars have long known that fishing eventually turns men into philosophers. Unfortunately, it is almost impossible to buy decent tackle on a philosopher’s salary',
+      person: 'Patrick McManus'
+    },
+    {
+      quote: `The fish and I were both stunned and disbelieving to find ourselves connected by a line.`,
+      person: 'William Humphrey'
+    },
+    {
+      quote: 'I’ve gone fishing thousands of times in my life, and I have never once felt unlucky or poorly paid for those hours on the water.',
+      person: 'William Tapply'
+    },
+    {
+      quote: 'I look into… my fly box, and think about all the elements I should consider in choosing the perfect fly: water temperature, what stage of development the bugs are in, what the fish are eating right now. Then I remember what a guide told me: ‘Ninety percent of what a trout eats is brown and fuzzy and about five-eighths of an inch long.',
+      person: 'Allison Moir'
+    },
+    {
+      quote: 'Angling is extremely time consuming. That’s sort of the whole point.',
+      person: 'Thomas McGuane'
+    },
+    {
+      quote: 'In every species of fish I’ve angled for, it is the ones that have got away that thrill me the most, the ones that keep fresh in my memory. So I say it is good to lose fish. If we didn’t, much of the thrill of angling would be gone',
+      person: 'Ray Bergman'
+    },
+    {
+      quote: 'The best fisherman I know try not to make the same mistakes over and over again; instead they strive to make new and interesting mistakes and to remember what they learned from them.',
+      person: 'John Gierach'
+    },
+    {
+        quote: 'A trout is a moment of beauty known only to those who seek it.',
+        person: 'Arnold Gingrich'
+    },
+    {
+        quote: 'It is impossible to grow weary of a sport that is never the same on any two days of the year.',
+        person: 'Theodore Gordon'
+    },
+    {
+        quote: 'There’s more B.S. in fly fishing than there is in a Kansas feedlot.',
+        person: 'Lefty Kreh'
+    },
+    {
+        quote: 'If I’m not going to catch anything, then I ‘d rather not catch anything on flies',
+        person: 'Bob Lawless'
+    },
+    {
+        quote: 'The water you touch in a river is the last of that which has passed, and the first of that which is coming; thus it is with time.',
+        person: 'Leonardo DaVinci'
+    },
+    {
+        quote: 'There will be days when the fishing is better than one’s most optimistic forecast, others when it is far worse. Either is a gain over just staying home.',
+        person: 'Roderick Haig-Brown'
+    },
+    {
+        quote: 'To him, all good things—trout as well as eternal salvation—come by grace and grace comes by art and art does not come easy.',
+        person: 'Norman Maclean'
+    },
+  ];
+  
+  module.exports = {
+    quotes
+  };
+  
